f72914336654ad4d0079f0be3906295154f9eade
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-04 00:45:22 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,20 +1,16 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
+epic_tragedy = {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
-      
-    :capulet => {
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
+      :heroine => {},
       :heroine_friends => []
-      }
-    
-  }
-
-  
+   }
+}
 end
